984bdaac73134ea37c592ea01c2b147eb327ab63
SeolDonghun <ssalssi1@gmail.com>
2023-07-06 14:44:05 +0900

    Add platform font config

--- app/components/graph_tab.py
+++ app/components/graph_tab.py
@@ -1,9 +1,19 @@
+import platform
+
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
 from utils.formatters import insert_commas
+
+# 운영체제별 matplotlib font 설정
+if platform.system() == 'Windows':
+    plt.rcParams['font.family'] = 'Malgun Gothic'
+elif platform.system() == 'Darwin':
+    plt.rcParams['font.family'] = 'AppleGothic'
+elif platform.system() == 'Linux':
+    plt.rcParams['font.family'] = 'NanumGothic'
 
 
 @st.cache_data(show_spinner='그래프를 렌더링 중입니다.')
