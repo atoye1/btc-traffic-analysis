0dcc7ce3cf49457c68f178a47c4a29f1bf08712a
SeolDonghun <ssalssi1@gmail.com>
2023-07-07 14:28:12 +0900

    add constants and in-app data files

--- app/components/init_session_states.py
+++ app/components/init_session_states.py
@@ -1,27 +1,29 @@
 import streamlit as st
-import datetime
 
 from utils.station_id_dict import station_id_dict
+from utils.constants import *
 
 
 def init_session_states():
     if 'start_date' not in st.session_state:
-        st.session_state['start_date'] = datetime.date(2014, 1, 1)
-    if 'end_date' not in st.session_state:
-        st.session_state['end_date'] = datetime.date(2023, 1, 1)
+        st.session_state['start_date'] = DEFAULT_START_DATE
     if 'start_time' not in st.session_state:
-        st.session_state['start_time'] = datetime.time(7, 0, 0)
+        st.session_state['start_time'] = DEFAULT_START_TIME
+    if 'end_date' not in st.session_state:
+        st.session_state['end_date'] = DEFAULT_END_DATE
     if 'end_time' not in st.session_state:
-        st.session_state['end_time'] = datetime.time(23, 0, 0)
+        st.session_state['end_time'] = DEFAULT_END_TIME
     if 'selected_lines' not in st.session_state:
-        st.session_state['selected_lines'] = ['전체']
+        st.session_state['selected_lines'] = DEFAULT_SELECTED_LINES
     if 'selected_stations' not in st.session_state:
-        st.session_state['selected_stations'] = ['전체']
-    if 'on_offs' not in st.session_state:
-        st.session_state['on_offs'] = '전체'
+        st.session_state['selected_stations'] = DEFAULT_SELECTED_STATIONS
+    if 'on_off' not in st.session_state:
+        st.session_state['on_off'] = DEFAULT_ON_OFF
     if 'filter_selection' not in st.session_state:
-        st.session_state['filter_selection'] = '일반 필터'
+        st.session_state['filter_selection'] = DEFAULT_FILTER_SELECTION
+    if 'chatbot_filter_input' not in st.session_state:
+        st.session_state['chatbot_filter_input'] = DEFAULT_CHATBOT_FILTER_INPUT
+    if 'chatbot_filter_output' not in st.session_state:
+        st.session_state['chatbot_filter_output'] = DEFAULT_CHATBOT_FILTER_OUTPUT
     if 'station_dict' not in st.session_state:
         st.session_state['station_dict'] = station_id_dict
-    if 'chatbot_filter_input' not in st.session_state:
-        st.session_state['chatbot_filter_input'] = ''
