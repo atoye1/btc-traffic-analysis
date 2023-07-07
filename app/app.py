30923c0c3f35fd792153f8bdfff8319b566a9e88
SeolDonghun <ssalssi1@gmail.com>
2023-07-07 14:28:47 +0900

    add intro messages,

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import warnings
 
 from components.data_tab import render_data_tab
 from components.analysis_tab import render_analysis_tab
@@ -9,6 +10,9 @@ from components.init_session_states import init_session_states
 
 from utils.load_raw_data import load_raw_data
 from utils.filter_dataframe import filter_dataframe
+
+
+warnings.filterwarnings('ignore')
 
 st.set_page_config(
     page_title="BTC_Traffic_Data",
@@ -25,10 +29,27 @@ df_raw = load_raw_data()
 render_sidebar(df_raw)
 
 # main section
-st.title("부산교통공사 시간대별 승하차 데이터")
-st.write("아래의 탭에서 원하는 기능을 선택하세요")
+st.title("부산교통공사 시간대별 승하차 데이터",
+         help='출처 : https://www.data.go.kr/data/3057229/fileData.do')
+st.markdown('부산교통공사에서 제공하는 시간대별 승하차 데이터를 조회, 분석합니다.')
+st.markdown('인공지능을 활용한 자연어 질의도 가능합니다.')
+st.write('### 사용법')
+st.write('1. 왼쪽의 사이드바에서 조회하고 싶은 데이터의 기간과 호선, 역, 승하차를 선택합니다.')
+st.write('2. 또는 인공지능 필터를 선택하고, 자연어로 조회하고 싶은 데이터를 입력합니다.')
+st.write('3. 필터링된 데이터를 확인하고, 아래의 탭을 탐색해보세요')
 
-df_filtered = filter_dataframe(df_raw)
+# st.cache_data 를 적용하기 위해 세션에 저장된 값을 각각 전달.
+df_filtered = filter_dataframe(df=df_raw,
+                               filter_selection=st.session_state['filter_selection'],
+                               start_date=st.session_state['start_date'],
+                               start_time=st.session_state['start_time'],
+                               end_date=st.session_state['end_date'],
+                               end_time=st.session_state['end_time'],
+                               selected_lines=st.session_state['selected_lines'],
+                               selected_stations=st.session_state['selected_stations'],
+                               on_off=st.session_state['on_off'],
+                               chatbot_filter_output=st.session_state['chatbot_filter_output'],
+                               )
 tab1, tab2, tab3, tab4, tab5 = st.tabs(
     ["데이터", "분석", "그래프", "AI에게 질문하기", "전체 데이터 분석"])
 with tab1:
